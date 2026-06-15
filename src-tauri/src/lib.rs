04dbffc4f5aa663701aea25e7def004da651fbaf
Dheeraj <dheerajyadav.me@gmail.com>
2026-06-15 19:21:16 +0530

    App build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+mod cert;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
@@ -11,6 +13,14 @@ pub fn run() {
       }
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![
+      cert::cert_verify_chain,
+      cert::cert_match_key,
+      cert::cert_match_csr,
+      cert::cert_generate_csr,
+      cert::cert_generate,
+      cert::cert_to_pkcs12,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
